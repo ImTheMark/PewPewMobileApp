b536c692fa755a74d14fef95b895151962544361
obs619 <oliversyson@yahoo.com>
2014-05-11 02:23:44 +0800

    rf

--- pewpewmobileapp/OrganizationPage.xaml.cs
+++ pewpewmobileapp/OrganizationPage.xaml.cs
@@ -9,19 +9,42 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using pewpewmobileapp.Objects;
 using System.Threading.Tasks;
+using System.Device.Location;
+using Microsoft.Phone.Maps.Services;
+using Windows.Devices.Geolocation;
+using System.Text;
+using Microsoft.Phone.Tasks;
+using Microsoft.Phone.Maps.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Diagnostics;
 
 namespace pewpewmobileapp
 {
     public partial class OrganizationPage : PhoneApplicationPage
     {
+
+        RouteQuery routeQuery = new RouteQuery();
+        List<GeoCoordinate> waypoints;
+        Geolocator geolocator;
+        StringBuilder sb = new StringBuilder();
+
         public OrganizationPage()
         {
             InitializeComponent();
+            geolocator = new Geolocator();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
+            routeQuery.TravelMode = TravelMode.Walking;
+            routeQuery.QueryCompleted += routeQuery_QueryCompleted;
+            GetCurrentLocation();
+            MyMapControl.CartographicMode = MapCartographicMode.Road;
+            MyMapControl.LandmarksEnabled = true;
+            MyMapControl.ZoomLevel = 16;
+
             int x = myAssets.Children.Count;
             for (int i = 0; i < x; i++)
                 myAssets.Children.RemoveAt(0);
@@ -30,6 +53,125 @@ namespace pewpewmobileapp
                 requestedAssets.Children.RemoveAt(0);
             loadRequestList();
             loadMyAssetList();
+        }
+
+        private async void GetCurrentLocation()
+        {
+            try
+            {
+                geolocator.DesiredAccuracyInMeters = 50;
+                geolocator.DesiredAccuracy = Windows.Devices.Geolocation.PositionAccuracy.High;
+                // If the cached location is over 30 minutes old, get a new one
+                TimeSpan maximumAge = new TimeSpan(0, 30, 0);
+                // If we're trying for more than 45 seconds, abandon the operation
+                TimeSpan timeout = new TimeSpan(0, 0, 15);
+                Geoposition myLocation = await geolocator.GetGeopositionAsync(maximumAge, timeout);
+
+                Debug.WriteLine("Latitude1: " + myLocation.Coordinate.Latitude + " Longitude1: " + myLocation.Coordinate.Longitude);
+
+                //  GeoCoordinate geoCord = new GeoCoordinate(myLocation.Coordinate.Latitude, myLocation.Coordinate.Longitude);
+                GeoCoordinate geoCord = new GeoCoordinate(14.5585541, 121.0181178);
+
+                waypoints = new List<GeoCoordinate>();
+                waypoints.Add(geoCord);
+                MyMapControl.Center = geoCord;
+                AddPoint(MyMapControl, geoCord);
+
+                geolocator.PositionChanged += newPosition;
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception);
+            }
+        }
+
+        void newPosition(Geolocator sender, PositionChangedEventArgs args)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                GeoCoordinate geoCord =
+                    new GeoCoordinate(args.Position.Coordinate.Latitude,
+                                        args.Position.Coordinate.Longitude);
+                MyMapControl.Center = geoCord;
+            });
+        }
+
+        bool isMappingRoute = false;
+
+        private void AddRoute(object sender, System.EventArgs e)
+        {
+            isMappingRoute = true;
+        }
+
+        private void GetGeolocationOfTap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (isMappingRoute)
+            {
+                Point tapPosition = e.GetPosition((UIElement)sender);
+                GeoCoordinate tapLocation = MyMapControl.ConvertViewportPointToGeoCoordinate(tapPosition);
+                waypoints.Add(tapLocation);
+                AddPoint(MyMapControl, tapLocation);
+            }
+
+        }
+
+        private void AddPoint(Map controlMap, GeoCoordinate geo)
+        {
+            MapLayer ml = new MapLayer();
+            MapOverlay mo = new MapOverlay();
+
+            // Add an Ellipse UI
+            Ellipse r = new Ellipse();
+            r.Fill = new SolidColorBrush(Color.FromArgb(255, 240, 5, 5));
+            r.Width = r.Height = 12;
+            r.Margin = new Thickness(-6, -6, 0, 0);
+            mo.Content = r;
+            mo.GeoCoordinate = geo;
+            ml.Add(mo);
+            controlMap.Layers.Add(ml);
+
+            Debug.WriteLine("Latitude: " + geo.Latitude + " Longitude: " + geo.Longitude);
+        }
+
+        private void MappingRoute(object sender, System.EventArgs e)
+        {
+            isMappingRoute = false;
+            routeQuery.Waypoints = waypoints;
+            routeQuery.QueryAsync();
+        }
+
+        private void Direction(object sender, System.EventArgs e)
+        {
+            MessageBox.Show(sb.ToString());
+        }
+
+        void routeQuery_QueryCompleted(object sender, QueryCompletedEventArgs<Route> e)
+        {
+            if (null == e.Error)
+            {
+                Route MyRoute = e.Result;
+                MapRoute MyMapRoute = new MapRoute(MyRoute);
+                MyMapControl.AddRoute(MyMapRoute);
+                MyMapControl.SetView(MyMapRoute.Route.BoundingBox);
+
+                var i = 0;
+                sb.AppendFormat("Estimated time: {0} minutes\n", e.Result.EstimatedDuration.TotalMinutes.ToString("#,###.##"));
+                foreach (var leg in e.Result.Legs)
+                    foreach (var maneuver in leg.Maneuvers)
+                    {
+                        sb.AppendFormat("{0}. {1}: {2}\n", ++i, maneuver.InstructionKind.ToString(), maneuver.InstructionText);
+                    }
+            }
+
+            else
+                MessageBox.Show("Error occured:\n" + e.Error.Message);
+
+        }
+
+        private void DownloadMapsForOffline(object sender, System.EventArgs e)
+        {
+            MapDownloaderTask mdt = new MapDownloaderTask();
+            mdt.Show();
         }
 
         private async void loadMyAssetList()
@@ -113,7 +255,12 @@ namespace pewpewmobileapp
 
         private void plotPoint(double p1, double p2)
         {
-            throw new NotImplementedException();
+            GeoCoordinate geoCord = new GeoCoordinate(p1, p2);
+
+            waypoints = new List<GeoCoordinate>();
+            waypoints.Add(geoCord);
+            MyMapControl.Center = geoCord;
+            AddPoint(MyMapControl, geoCord);
         }
 
         private async Task<Town> getBorrower(string id)
