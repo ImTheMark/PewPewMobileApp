1bd3985afa211a54927776f6b90bef4b9a879915
ImTheMark <mark_serapion@yahoo.com>
2014-05-11 00:15:41 +0800

    Reverted home

--- pewpewmobileapp/OrganizationPage.xaml.cs
+++ pewpewmobileapp/OrganizationPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using pewpewmobileapp.Objects;
+using System.Threading.Tasks;
 
 namespace pewpewmobileapp
 {
@@ -31,18 +32,54 @@ namespace pewpewmobileapp
             loadMyAssetList();
         }
 
-        private async void loadRequestList()
+        private async void loadMyAssetList()
         {
             var list = await App.MobileService.GetTable<Asset>().ToListAsync();
             foreach (Asset asset in list)
             {
-                if (asset.owner.Equals(MainPage.userID) && asset.status.Equals("ON THE WAY"))
+                //if (asset.owner.Equals(MainPage.userID) && asset.status.Equals("ON THE WAY"))
+                //{ 
+                //}
+            }
+        }
+
+        private async void loadRequestList()
+        {
+            loadMaps();
+            var list = await App.MobileService.GetTable<Asset>().ToListAsync();
+            foreach (Asset asset in list)
+            {
+                /*if (asset.owner.Equals(MainPage.userID) && asset.status.Equals("ON THE WAY"))
                 {
+                    Town borrower = await getBorrowerAddress(asset.borrower);
                     Button b = new Button();
                     b.Name = asset.id;
-                    b.
+                    b.Content = asset.name + "\n" + asset.borrower + "\n"+ borrower.address;
+                    b.Click += (s, e) =>
+                    {
+                        NavigationService.Navigate(new Uri("/AssetPage.xaml?param0=" + asset.id + "&param1=" + "REQUEST DETAIL", UriKind.Relative));
+                    };
+                    requestedAssets.Children.Add(b);
+                }*/
+            }
+        }
+
+        private void loadMaps()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<Town> getBorrowerAddress(string id)
+        {
+            var list = await App.MobileService.GetTable<Town>().ToListAsync();
+            foreach (Town t in list)
+            {
+                if (t.id.Equals(id))
+                {
+                    return t;
                 }
             }
+            return null;
         }
     }
 }
