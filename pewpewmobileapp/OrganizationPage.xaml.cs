750aeb67880d3bce75369f4c528e6fc0b1503e0d
ImTheMark <mark_serapion@yahoo.com>
2014-05-11 00:41:20 +0800

    Organization Panorama Details

--- pewpewmobileapp/OrganizationPage.xaml.cs
+++ pewpewmobileapp/OrganizationPage.xaml.cs
@@ -34,24 +34,69 @@ namespace pewpewmobileapp
 
         private async void loadMyAssetList()
         {
+            StackPanel truckList = new StackPanel();
+            StackPanel lifevestList = new StackPanel();
+            StackPanel doctorList = new StackPanel();
+            StackPanel rubberBoatList = new StackPanel();
+            StackPanel fireTruckList = new StackPanel();
+            StackPanel militaryList = new StackPanel();
+            StackPanel socialWorkerList = new StackPanel();
+            StackPanel tentList = new StackPanel();
+            StackPanel radioList = new StackPanel();
+            StackPanel constructionEquipmentsList = new StackPanel();
+            StackPanel helicopterList = new StackPanel();
+            StackPanel generatorsList = new StackPanel();
             var list = await App.MobileService.GetTable<Asset>().ToListAsync();
             foreach (Asset asset in list)
             {
-                //if (asset.owner.Equals(MainPage.userID) && asset.status.Equals("ON THE WAY"))
-                //{ 
-                //}
+                if (asset.owner.Equals(MainPage.userID))
+                {
+                    Button b = new Button();
+                    b.Name = asset.id;
+                    b.Content = asset.name + "\n" + asset.borrower + "\n" + asset.status;
+                    b.Click += (s, e) =>
+                    {
+                        NavigationService.Navigate(new Uri("/AssetPage.xaml?param0=" + asset.id + "&param1=" + "REQUEST DETAIL", UriKind.Relative));
+                    };
+                    switch (asset.id)
+                    {
+                        case "truck":
+                            truckList.Children.Add(b); break;
+                        case "lifevest":
+                            lifevestList.Children.Add(b); break;
+                        case "doctor":
+                            doctorList.Children.Add(b); break;
+                        case "rubberBoat":
+                            rubberBoatList.Children.Add(b); break;
+                        case "fireTruck":
+                            fireTruckList.Children.Add(b); break;
+                        case "military":
+                            militaryList.Children.Add(b); break;
+                        case "socialWorker":
+                            socialWorkerList.Children.Add(b); break;
+                        case "tent":
+                            tentList.Children.Add(b); break;
+                        case "radio":
+                            radioList.Children.Add(b); break;
+                        case "constructionEquipments":
+                            constructionEquipmentsList.Children.Add(b); break;
+                        case "helicopter":
+                            helicopterList.Children.Add(b); break;
+                        case "generators":
+                            generatorsList.Children.Add(b); break;
+                    }
+                }
             }
         }
 
         private async void loadRequestList()
         {
-            loadMaps();
             var list = await App.MobileService.GetTable<Asset>().ToListAsync();
             foreach (Asset asset in list)
             {
-                /*if (asset.owner.Equals(MainPage.userID) && asset.status.Equals("ON THE WAY"))
+                if (asset.owner.Equals(MainPage.userID) && asset.status.Equals("ON THE WAY"))
                 {
-                    Town borrower = await getBorrowerAddress(asset.borrower);
+                    Town borrower = await getBorrower(asset.borrower);
                     Button b = new Button();
                     b.Name = asset.id;
                     b.Content = asset.name + "\n" + asset.borrower + "\n"+ borrower.address;
@@ -59,17 +104,19 @@ namespace pewpewmobileapp
                     {
                         NavigationService.Navigate(new Uri("/AssetPage.xaml?param0=" + asset.id + "&param1=" + "REQUEST DETAIL", UriKind.Relative));
                     };
+                    Organization owner = await getOwner(asset.owner);
+                    plotPoint(owner.latitude, owner.longitude);
                     requestedAssets.Children.Add(b);
-                }*/
+                }
             }
         }
 
-        private void loadMaps()
+        private void plotPoint(double p1, double p2)
         {
             throw new NotImplementedException();
         }
 
-        private async Task<Town> getBorrowerAddress(string id)
+        private async Task<Town> getBorrower(string id)
         {
             var list = await App.MobileService.GetTable<Town>().ToListAsync();
             foreach (Town t in list)
@@ -77,6 +124,19 @@ namespace pewpewmobileapp
                 if (t.id.Equals(id))
                 {
                     return t;
+                }
+            }
+            return null;
+        }
+
+        private async Task<Organization> getOwner(string id)
+        {
+            var list = await App.MobileService.GetTable<Organization>().ToListAsync();
+            foreach (Organization o in list)
+            {
+                if (o.id.Equals(id))
+                {
+                    return o;
                 }
             }
             return null;
