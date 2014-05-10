3b2c80020ef1748deb6183b995655ab501be6e10
Zeroh729 <joyceplayingcp@yahoo.com>
2014-05-11 05:35:17 +0800

    committed the requestapproval

---
+++ pewpewmobileapp/ApprovalRequest.cs
@@ -0,0 +1,59 @@
+﻿using pewpewmobileapp.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pewpewmobileapp
+{
+    class ApprovalRequest
+    {
+
+        public async Task<List<Asset>> GetListOfAsset()
+        {
+            List<Asset> listAsset = await App.MobileService.GetTable<Asset>().ToListAsync();
+            return listAsset;
+        }
+
+        public async Task<Asset> FindAvailableAsset(string name)
+        {
+            List<Asset> assets = await GetListOfAsset();
+            foreach (Asset asset in assets)
+            {
+                if (asset.status.Equals("AVAILABLE") && asset.name.Equals(name))
+                {
+                    return asset;
+                }
+            }
+
+            return null;
+        }
+
+
+        public async void RequestAsset(int quantity, string assetName, Town requester)
+        {
+            List<Asset> assets = await GetListOfAsset();
+            for (int i = 0; i < quantity; i++)
+            {
+                Asset foundAsset = await FindAvailableAsset(assetName);
+                if (foundAsset != null)
+                {
+                    foundAsset.borrower = requester.contactperson;
+                    foundAsset.status = "ON THE WAY";
+                    await App.MobileService.GetTable<Asset>().UpdateAsync(foundAsset);
+                }
+                else
+                {
+                    Pending pendingticket = new Pending();
+                    pendingticket.assetName = assetName;
+                    pendingticket.townID = requester.id;
+                    await App.MobileService.GetTable<Pending>().InsertAsync(pendingticket);
+                }
+            }
+
+        }
+
+        
+    }
+}
